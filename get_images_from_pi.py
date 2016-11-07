0eb781a10064e5826dd259e973840723cff1c461
farrellw <farrell.will.g@gmail.com>
2016-11-07 15:16:54 -0600

    Add relative path to analyze pic server

--- get_images_from_pi.py
+++ get_images_from_pi.py
@@ -1,8 +1,7 @@
 import os
 import time
 
-
-def get_image_x_times(times)
+def get_image_x_times(times):
 	count = 0
 	while count < times:
 		#replace sleep with the amount of time needed to sleep in between getting pictures.
@@ -10,9 +9,8 @@ def get_image_x_times(times)
 		count += 1
 
 def get_image():
-		os.system('scp "%s:%s" "%s"' % ('pi@192.168.2.5', 'Desktop/greg.jpg', 'greg.jpg') )
+		os.system('scp "%s:%s" "%s" "%s"' % ('pi@192.168.2.5', 'Desktop/greg.jpg', 'greg.jpg', os.getcwd() + '/images') )
 		#if pi address isn't connecting right away, need to create a key.  Use the following website to do so
 		#https://www.raspberrypi.org/documentation/remote-access/ssh/passwordless.md
 		#replace Desktop/greg with the path.
 		#replace greg with the file name
-		
--- models/analyze_pic_server.py
+++ models/analyze_pic_server.py
@@ -1,5 +1,9 @@
-import analyze
+# import analyze
 from analyze import check_blob_in_direct_path
 import os
+import sys
+sys.path.insert(0, os.getcwd())
+from get_images_from_pi import get_image, get_image_x_times
 
-check_blob_in_direct_path(os.getcwd() + "/images/two.jpg")
+get_image()
+check_blob_in_direct_path(os.getcwd() + "/images/greg.jpg")
