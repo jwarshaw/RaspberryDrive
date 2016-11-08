e9487399263c05fe9bb67fbaf443d8a8305001dc
Greg Garner <email": null>
2016-11-08 08:42:05 -0600

    Add psuedocode logic

--- controllers/controller.py
+++ controllers/controller.py
@@ -8,16 +8,18 @@ from models.analyze import check_blob_in_direct_path
 from get_images_from_pi import get_image, valid_image
 from connect import new_connection, send_command, receive_confirmation
 
-#controller action
-#start a server
-#x times do:
-  #get image, check if its in path.
-  #send instruction over server
-  #sleep, do it again
-count = 0
+#start the camera taking pictures on contr pi
+#start the server listening on camera pi
+#start the server connection on this current server
+
+
 s = new_connection()
+
+#x times do:
+  #retrieve image.  mage, check if its in path.
+  #send instruction over server
+count = 0
 while (count < 100):
-  instruction = check_blob_in_direct_path(os.getcwd() + "/images/gregTest.jpg")
   get_image()
   if valid_image(os.getcwd() + "/images/gregTest.jpg"):
     instruction = check_blob_in_direct_path(os.getcwd() + "/images/gregTest.jpg")
