1e6fda9cbb8984dd09261636f03e408176bfbf86
Greg Garner <email": null>
2016-11-09 09:56:36 -0600

    Reorganize driver code in controller into methods

--- controllers/controller.py
+++ controllers/controller.py
@@ -22,40 +22,47 @@ def start_camera_thread():
 	camera_thread.start()
 	return;
 
+def start_all():
+  start_server_thread()
+  start_camera_thread()
+  sleep(7)
+  s = new_connection()
+  return s;
+  #start the server listening on contr pi
+  #start the camera taking pictures on camera pi
+  # Sleep to make sure server connects
+  #start the server connection on this current server
 
-#Start-all
-#start the server listening on contr pi
-start_server_thread()
-#start the camera taking pictures on camera pi
-start_camera_thread()
-# Sleep to make sure server connects
-sleep(7)
-#start the server connection on this current server
-s = new_connection()
+def end_all(server):
+  send_end_connection(server)
+  server.close()
+  return;
+  #End-all
+  #Camera exits automatically automatically
+  #Close the Server Connection on the Pi-Controller End
+  #Kill the server listening process
 
-#Run-All.
-count = 0
-while (count < 20):
-  instruction = "forward"
-  #retrieve image.
-  get_image(count)
-  if valid_image(os.getcwd() + "/gregTest.jpg"):
-  	#Blog Detection
-    in_way = runAnalyzeLines(os.getcwd() + "/gregTest.jpg")
-    if (in_way == True):
-      instruction = "stop"
-  	#send instruction over server
-    send_command(s, instruction, "0.5")
-    #confirmation that instruction was received
-    receive_confirmation(s)
-    print instruction
-  count += 1
+def run(server):
+  count = 0
+  while (count < 20):
+    instruction = "forward"
+    #retrieve image.
+    get_image(count)
+    if valid_image(os.getcwd() + "/gregTest.jpg"):
+      #Blog Detection
+      in_way = runAnalyzeLines(os.getcwd() + "/gregTest.jpg")
+      if (in_way == True):
+        instruction = "stop"
+      #send instruction over server
+      send_command(server, instruction, "0.5")
+      #confirmation that instruction was received
+      receive_confirmation(server)
+      print instruction
+    count += 1
+  return;
 
-#End-all
-#Camera exits automatically automatically
-#Close the Server Connection on the Pi-Controller End
-send_end_connection(s)
-#Kill the server listening process
-s.close()
+server = start_all()
+run(server)
+end_all(server)
 #exit out of controller
 exit()
