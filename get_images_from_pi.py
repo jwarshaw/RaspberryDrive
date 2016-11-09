4ba0578efda94da0f9a1bfc187418a116479697e
Greg Garner <email": null>
2016-11-09 09:45:53 -0600

    Commented out code explaining

--- get_images_from_pi.py
+++ get_images_from_pi.py
@@ -6,9 +6,7 @@ def get_image(count):
   os.system(scp_command)
   return
 	# os.system('scp pi@192.168.2.5:Desktop/gregTest.jpg gregTest.jpg')
-
-	#if on a new computer and the pi address isn't connecting right away, need to create a key.  Use the following website to do so
-	#https://www.raspberrypi.org/documentation/remote-access/ssh/passwordless.md
+  #use following to create key if needed:  https://www.raspberrypi.org/documentation/remote-access/ssh/passwordless.md
 
 def valid_image(path):
 	try:
