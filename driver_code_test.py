74bff7f21a859ab54e8b507fd5f130130c45d8d1
Greg Garner <email": null>
2016-11-09 11:20:19 -0600

    Add logic to test pictures

--- driver_code_test.py
+++ driver_code_test.py
@@ -1,10 +1,31 @@
-from get_images_from_pi import get_image, valid_image
+import SimpleCV
+from SimpleCV import Image
+import cv2
+import time
+from start_camera import start_camera
+import threading
 
-count = 0
-while (count < 10):
-  get_image(count)
-  count += 1
+# camera_thread = threading.Thread(target=start_camera)
+# camera_thread.start()
+# from get_images_from_pi import get_image, valid_image
 
-exit()
+# count = 0
+# while (count < 10):
+#   get_image(count)
+#   count += 1
+
+# exit()
+
+image = Image('images/1.jpg')
+blobs = image.findBlobs()
+for blob in blobs:
+  blob.draw(color=(0, 128, 0))
+image.show()
+image.show()
+time.sleep(4)
+
+
+
+
 
 
