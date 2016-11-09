e2c33e332aa5e2071f470d7777c166a15c966f3d
Greg Garner <email": null>
2016-11-09 11:46:19 -0600

    Add easy driver code to driver_code_test for getting and testing images

--- driver_code_test.py
+++ driver_code_test.py
@@ -5,24 +5,31 @@ import time
 from start_camera import start_camera
 import threading
 
-# camera_thread = threading.Thread(target=start_camera)
-# camera_thread.start()
-# from get_images_from_pi import get_image, valid_image
+camera_thread = threading.Thread(target=start_camera)
+camera_thread.start()
+from get_images_from_pi import get_image, valid_image
 
-# count = 0
-# while (count < 10):
-#   get_image(count)
-#   count += 1
+count = 0
+while (count < 10):
+  get_image(count)
+  count += 1
 
-# exit()
+exit()
 
-image = Image('images/1.jpg')
-blobs = image.findBlobs()
-for blob in blobs:
-  blob.draw(color=(0, 128, 0))
-image.show()
-image.show()
-time.sleep(4)
+# image = Image('images/9.jpg')
+# # p = image.getPalette()
+# # rgb = PIL.ImageColor.getrgb("blue")
+# # print rgb
+# blobs = image.findBlobs(threshval=(0,0,255))
+# # blobs = image.findBlobsFromPalette( (p[0],p[1],p[2]) )
+# # blobs = image.findBlobsFromHueHistogram((0,0,50,50))
+# if blobs:
+#   for blob in blobs:
+#     print "got a blob"
+#     blob.draw(color=(0, 128, 0))
+# image.show()
+# image.show()
+# time.sleep(4)
 
 
 
