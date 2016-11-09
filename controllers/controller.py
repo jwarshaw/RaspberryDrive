2f64aaa19187f5a60fe48268e91349c402c0b1a9
Greg Garner <email": null>
2016-11-08 18:59:12 -0600

    To merge

--- controllers/controller.py
+++ controllers/controller.py
@@ -14,43 +14,43 @@ from start_server import start_server
 import threading
 
 #Start-all
-threads = []
-#start the server listening on contr pi
-server_thread = threading.Thread(target=start_server)
-threads.append(server_thread)
-server_thread.start()
-#Sleep for testing
-sleep(2)
-#start the camera taking pictures on camera pi
-camera_thread = threading.Thread(target=start_camera)
-threads.append(camera_thread)
-camera_thread.start()
-# Sleep to make sure server connects
-sleep(5)
-#start the server connection on this current server
-s = new_connection()
+# threads = []
+# #start the server listening on contr pi
+# server_thread = threading.Thread(target=start_server)
+# threads.append(server_thread)
+# server_thread.start()
+# #Sleep for testing
+# sleep(2)
+# #start the camera taking pictures on camera pi
+# camera_thread = threading.Thread(target=start_camera)
+# threads.append(camera_thread)
+# camera_thread.start()
+# # Sleep to make sure server connects
+# sleep(5)
+# #start the server connection on this current server
+# s = new_connection()
 
-#Run-All.  X times do
-    #retrieve image.  #Blog Detection
-    #send instruction over server
-count = 0
-while (count < 20):
-  instruction = "forward"
-  get_image()
-  if valid_image(os.getcwd() + "/gregTest.jpg"):
-    in_way = runAnalyzeLines(os.getcwd() + "/gregTest.jpg")
-    if (in_way == True):
-      instruction = "stop"
-    send_command(s, instruction, "0.5")
-    receive_confirmation(s)
-    # sleep(0.25)
-    print instruction
-  count += 1
+# #Run-All.  X times do
+#     #retrieve image.  #Blog Detection
+#     #send instruction over server
+# count = 0
+# while (count < 20):
+#   instruction = "forward"
+#   get_image()
+#   if valid_image(os.getcwd() + "/gregTest.jpg"):
+#     in_way = runAnalyzeLines(os.getcwd() + "/gregTest.jpg")
+#     if (in_way == True):
+#       instruction = "stop"
+#     send_command(s, instruction, "0.5")
+#     receive_confirmation(s)
+#     # sleep(0.25)
+#     print instruction
+#   count += 1
 
-# #End-all
-#   #Stop the camera taking pictures on the camera pi -- still needs to be implemented
-#   #close the server connection
-send_end_connection(s)
-#   #Kill the server listening process
-s.close()
-exit()
+# # #End-all
+# #   #Stop the camera taking pictures on the camera pi -- still needs to be implemented
+# #   #close the server connection
+# send_end_connection(s)
+# #   #Kill the server listening process
+# s.close()
+# exit()
