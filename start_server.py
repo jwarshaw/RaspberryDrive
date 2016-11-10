2666eee0a59581c504b36acd618e256cf313c377
Greg Garner <email": null>
2016-11-10 09:13:50 -0600

    Add logic to try server 3 times, pausing a little more each time.

--- start_server.py
+++ start_server.py
@@ -1,6 +1,16 @@
 import os
 
 def start_server():
-  os.system('ssh pi@192.168.2.4 python python-libs/RaspberryDrive/driving_server.py &')
-  return
+  count = 0
+  while count < 2
+    send_ssh_server_start(count)
+    count +=1
+  exit()
+
+def send_ssh_server_start(count):
+  try:
+    os.system('ssh pi@192.168.2.4 python python-libs/RaspberryDrive/driving_server.py &')
+    return
+  except:
+    sleep(count + 1)
 
